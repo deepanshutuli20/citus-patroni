f7aa3bafdb7916a1a69fe41303491e22edc6751c
deepanshutuli20 <tulideepanshu@gmail.com>
2023-08-24 15:40:45 +0530

    revised changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+set -e
 
-if [ "$USE_PATRONI" = "true" ]; then
-    # Modify the Patroni template configuration using environment variables, if necessary
-    envsubst < /etc/patroni.yml.template > /etc/patroni.yml
-    exec ${VENV_PATH}/bin/patroni /etc/patroni.yml
-else
-    exec docker-entrypoint.sh postgres -c shared_preload_libraries=citus
+# The main entry point for Citus
+/docker-entrypoint-1.sh postgres &
+
+# Allow the database to start up
+sleep 10
+
+# Check if we're the coordinator
+if [ "$NODE_TYPE" = "coordinator" ]; then
+    # Create the Citus extension and add workers
+    psql -U postgres -c "CREATE EXTENSION citus;"
+    psql -U postgres -c "SELECT * from master_add_node('worker1', 5432);"
+    psql -U postgres -c "SELECT * from master_add_node('worker2', 5432);"
 fi
+
+# Run the command provided to docker run
+exec "$@"
